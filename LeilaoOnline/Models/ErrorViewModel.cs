37074efe002c7084931030f4304caf6ccb08aa46
tercini <raphael.tercini@hotmail.com>
2020-11-24 19:44:56 -0300

    Projeto criado

---
+++ LeilaoOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LeilaoOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
